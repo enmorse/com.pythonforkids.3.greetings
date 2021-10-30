001e6473a793fe52e1c498fb52781d5eecf1689a
Ernest Morse <enmorse48@gmail.com>
2021-10-30 12:30:02 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create two variables; one that is set equal to
+# hold your first name and one that is set equal to
+# hold your last name. Now create a string and use
+# place holders to print you name with a message
+# using two variables, such as "Hi there, So And
+# So."
+
+first_name = "ernest"
+last_name = "morse"
+
+greeting = f"Hello, {first_name.title()} " \
+           f"{last_name.title()}!"
+
+print(greeting)
